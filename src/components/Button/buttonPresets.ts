94224e6930768534ddcbb9c9847cd70cce25a0ef
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-21 01:16:12 -0300

    feat: add Button component & presets

---
+++ src/components/Button/buttonPresets.ts
@@ -0,0 +1,79 @@
+import { ThemeColors } from '@theme';
+import { TouchableOpacityBoxProps } from '../Box';
+
+import { ButtonPreset } from './index';
+
+interface ButtonUI {
+  container: TouchableOpacityBoxProps;
+  content: ThemeColors;
+}
+
+export const buttonPresets: Record<
+  ButtonPreset,
+  {
+    default: ButtonUI;
+    disabled: ButtonUI;
+  }
+> = {
+  primary: {
+    default: {
+      container: {
+        backgroundColor: 'background',
+      },
+      content: 'white',
+    },
+    disabled: {
+      container: {
+        backgroundColor: 'background',
+        activeOpacity: 0.4,
+      },
+      content: 'white',
+    },
+  },
+  outline: {
+    default: {
+      container: {
+        borderWidth: 1,
+        borderColor: 'blue',
+      },
+      content: 'white',
+    },
+    disabled: {
+      container: {
+        borderWidth: 1,
+        borderColor: 'blue',
+      },
+      content: 'white',
+    },
+  },
+  default: {
+    default: {
+      container: {
+        backgroundColor: 'blue',
+      },
+      content: 'white',
+    },
+    disabled: {
+      container: {
+        backgroundColor: 'blue',
+        activeOpacity: 0.4,
+      },
+      content: 'white',
+    },
+  },
+
+  danger: {
+    default: {
+      container: {
+        backgroundColor: 'red',
+      },
+      content: 'white',
+    },
+    disabled: {
+      container: {
+        backgroundColor: 'red',
+      },
+      content: 'white',
+    },
+  },
+};
