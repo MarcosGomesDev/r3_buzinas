9d1ba9644af889587e4872455aff2d68a982307b
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-21 01:23:11 -0300

    feat: export components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './ActivityIndicator';
+export * from './Box';
+export * from './Button';
+export * from './Icon';
+export * from './Screen';
+export * from './Text';
+export * from './TextInput';
