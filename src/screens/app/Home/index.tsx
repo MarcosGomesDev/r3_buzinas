2c126c62eb7ddf9dff9f35b704fa1d470139830a
Marcos Gomes <marcosgomesdev@gmail.com>
2024-06-22 02:58:22 -0300

    chore: add react-native-ble-manager connection

--- src/screens/app/Home/index.tsx
+++ src/screens/app/Home/index.tsx
@@ -1,8 +1,19 @@
-import { Box, Screen, Text } from '@components';
-import { useAuthCredentials } from '@services';
-import { Image } from 'react-native';
+import { Box, Button, Screen, Text } from '@components';
+import { handleAndroidPermissionCheck } from '@services';
+import { useEffect, useState } from 'react';
+import { Alert, Image, NativeEventEmitter, NativeModules } from 'react-native';
+import BleManager from 'react-native-ble-manager';
 
-function Header() {
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+interface Device {
+  id: string;
+  name: string | undefined;
+  isConnected?: boolean;
+}
+
+function Header({ onPress }: { onPress: () => void }) {
   return (
     <Box
       height={70}
@@ -10,6 +21,7 @@ function Header() {
       justifyContent="center"
       backgroundColor="primary"
       alignContent="center"
+      flexDirection="row"
       alignItems="center">
       <Image
         source={require('@assets/images/logo.png')}
@@ -26,19 +38,193 @@ function Header() {
         }}
         resizeMode="contain"
       />
+
+      <Button icon iconName="menu" title="" onPress={onPress} />
     </Box>
   );
 }
 
 export function HomeScreen() {
-  const { authCredentials } = useAuthCredentials();
+  const [isScanning, setIsScanning] = useState(false);
+  const [discoveredDevices, setDiscoveredDevices] = useState<Device[]>([]);
+  const [connectedDevices, setConnectedDevices] = useState<Device[]>([]);
 
-  console.log('authCredentials', authCredentials);
+  function handleGetConnectedDevices() {
+    // if (Platform.OS === 'android' && Platform.Version >= 26) {
+    //   BleManager.getAssociatedPeripherals().then(results => {
+    //     console.log('getAssociatedPeripherals', results);
+    //     const newConnectedDevices = results.map(result => ({
+    //       id: result.id,
+    //       name: result.name,
+    //     }));
+
+    //     setConnectedDevices(prevConnectedDevices => {
+    //       const deviceMap = new Map();
+
+    //       prevConnectedDevices.forEach(device =>
+    //         deviceMap.set(device.id, device),
+    //       );
+
+    //       newConnectedDevices.forEach(device =>
+    //         deviceMap.set(device.id, device),
+    //       );
+
+    //       return Array.from(deviceMap.values());
+    //     });
+    //   });
+    //   return;
+    // }
+
+    BleManager.getBondedPeripherals().then(results => {
+      console.log('getBondedPeripherals', results);
+      const newConnectedDevices = results.map(result => ({
+        id: result.id,
+        name: result.name,
+      }));
+
+      setConnectedDevices(prevConnectedDevices => [
+        ...prevConnectedDevices,
+        ...newConnectedDevices,
+      ]);
+    });
+  }
+
+  async function checkState(): Promise<boolean> {
+    let enabled = false;
+    await BleManager.checkState().then(async state => {
+      if (state === 'on') {
+        enabled = true;
+      }
+
+      if (state === 'off') {
+        await BleManager.enableBluetooth().then(() => {
+          enabled = true;
+        });
+      }
+    });
+
+    return enabled;
+  }
+
+  async function startScan() {
+    const enabled = await checkState();
+
+    if (!isScanning && enabled) {
+      BleManager.scan([], 5, false)
+        .then(async () => {
+          console.log('Scanning...');
+          setIsScanning(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }
+
+  function handleUseBluetooth() {
+    startScan();
+  }
+
+  useEffect(() => {
+    handleAndroidPermissionCheck(granted => granted);
+
+    checkState().then(enabled => {
+      if (enabled) {
+        BleManager.start({ showAlert: false }).then(() => {
+          handleGetConnectedDevices();
+        });
+      }
+    });
+
+    let stopDiscoverListener = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      peripheral => {
+        const newDevice = {
+          id: peripheral.id,
+          name: peripheral.name,
+        };
+
+        if (
+          !discoveredDevices.some(
+            device => device.id.toString() === peripheral.id.toString(),
+          )
+        ) {
+          setDiscoveredDevices(prevDiscoveredDevices => [
+            ...prevDiscoveredDevices,
+            newDevice,
+          ]);
+        }
+      },
+    );
+    let stopConnectListener = BleManagerEmitter.addListener(
+      'BleManagerConnectPeripheral',
+      () => {},
+    );
+    let stopScanListener = BleManagerEmitter.addListener(
+      'BleManagerStopScan',
+      () => {
+        setIsScanning(false);
+        console.log('scan stopped');
+      },
+    );
+    return () => {
+      stopDiscoverListener.remove();
+      stopConnectListener.remove();
+      stopScanListener.remove();
+    };
+  }, [discoveredDevices]);
+
+  async function disconnectFromDevice(device: Device) {
+    BleManager.removeBond(device.id)
+      .then(() => {
+        device.isConnected = false;
+        setConnectedDevices([]);
+        Alert.alert(`Disconnected from ${device.name}`);
+      })
+      .catch(() => {
+        console.log('fail to remove the bond');
+      });
+  }
+  async function connectToDevice(device: Device) {
+    await BleManager.createBond(device.id)
+      .then(() => {
+        device.isConnected = true;
+        console.log('BLE device paired successfully', device);
+      })
+      .catch(() => {
+        console.log('failed to bond');
+      });
+  }
 
   return (
-    <Screen hasHeader HeaderComponent={<Header />} style={{ paddingTop: 0 }}>
+    <Screen
+      hasHeader
+      HeaderComponent={<Header onPress={handleUseBluetooth} />}
+      style={{ paddingTop: 0 }}>
       <Box>
-        <Text>Home</Text>
+        <Text>{connectedDevices.length}</Text>
+
+        <Button
+          title="Connect"
+          onPress={() =>
+            connectToDevice({
+              id: '50:78:93:95:D2:EB',
+              name: 'LE-391',
+              isConnected: false,
+            })
+          }
+        />
+
+        <Button
+          title="Disconnected"
+          onPress={() =>
+            disconnectFromDevice({
+              id: '50:78:93:95:D2:EB',
+              name: 'LE-391',
+              isConnected: false,
+            })
+          }
+        />
       </Box>
     </Screen>
   );
